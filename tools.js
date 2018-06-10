b1f167c9c5afe7b8c25ab8ecc28136dfc86704eb
SergChernykh <sergeichernykh95@gmail.com>
2018-06-10 14:14:07 +0700

    * implemented loadBaseTable function

--- tools.js
+++ tools.js
@@ -1,20 +1,25 @@
 function loadBaseTable(element, coreOption) {
-  var xmlhttp, myObj, x, txt = "";
-    
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myObj = JSON.parse(this.responseText);
-        txt += "<table class='optTable'>"
+    var txt = "<table class='optTable'>";
 
-        for (x in myObj) {
-        txt += "<tr><td>" + myObj[x].name + "</td></tr>";
-      }
-      txt += "</table>"        
-      document.getElementById("demo").innerHTML = txt;
-    }
-  };
-  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xmlhttp.send("x=" + dbParam);
+    var planOptions = data[coreOption];
 
+    var addField = (field) => {
+        txt += "<td>" + field + "</td>";
+    };
+
+    var addButton = () => {
+        txt += "<td>" + "<label class='switch'><input type='checkbox'><span class='slider round'></span></label>" + "</td>";
+    };
+
+    planOptions.base.forEach(item => {
+        txt += "<tr>";
+        addField(item.name);
+        addField(item.desc);
+        addField(item.cost);
+        addButton();
+        txt += "</tr>";
+    });
+
+    txt += "</table>"
+    document.getElementById(element).innerHTML = txt;
 }
